a939100c786612e4e76f1cdba5e863060766cf51
ezequielfreire007 <ezequiel.a.freire@gmail.com>
2016-09-07 10:18:40 -0300

    Se agrega ejercicio n°17

---
+++ guia-ejercicios-php/aplicacion-17/clases/Auto.php
@@ -0,0 +1,24 @@
+<?php
+    /**
+     * Ejercicio N° 17
+     */
+    class Auto
+    {
+        #Atributos
+        private $_color;
+        private $_precio;
+        private $_marca;
+        private $_fecha;
+
+        #Getter y Setter
+
+        #Constructor
+        public function __construct()
+        {
+        
+        }
+
+        #Metodo
+    }
+
+ ?>
