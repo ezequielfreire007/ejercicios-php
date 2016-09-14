becd7c62116a3107a7ccd89b25f19514c88a43ab
ezequielfreire007 <ezequiel.a.freire@gmail.com>
2016-09-13 22:49:45 -0300

    ejercicio 20 y 23

--- guia-ejercicios-php/aplicacion-23/index.php
+++ guia-ejercicios-php/aplicacion-23/index.php
@@ -1,30 +1,38 @@
-
 <?php
 
+	$color = "";
 
 	if(isset($_GET["opcion"])){
-    	$color = $_GET["opcion"];
-	}
+    	//$color = $_GET["opcion"];
+	
+    	if ($_GET["opcion"] == "red") {
+    		$color = "red";
+    	}elseif ($_GET["opcion"] == "blue") {
+    		$color = "blue";
+    	}elseif ($_GET["opcion"] == "green") {
+    		$color = "green";
+    	}
 
+	}
 
 ?>
 
 <html>
 	<head>
-		<title>Ejercicio nº23</title>
+		<title>Ejercicio nº23</title>	
 	</head>
 
 	<body style=background-color:<?php echo $color;?>>
 		<form action="index.php" method="get">
 			<select name ="opcion">
 				<optgroup label="Colores">
-					<option value="red">Blanco</option>
-					<option value="blue">Rojo</option>
-					<option value="green">Azul</option>
+					<option value="red">Rojo</option>
+					<option value="blue">Azul</option>
+					<option value="green">Verde</option>
 				</optgroup>
 			</select>
-			<input type="submit" value="Enviar"/>
-		</form>
+			<input type="submit" value="Cambiar Color"/>
+		</form>	
 	</body>
 
 </html>
