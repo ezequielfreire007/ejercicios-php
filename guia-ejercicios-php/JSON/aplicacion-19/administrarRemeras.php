8de1b7552a89ea9fc9bb7fa42727295e0aa4a768
ezequielfreire007 <ezequiel.a.freire@gmail.com>
2016-10-29 14:36:34 -0300

    modificacion json 19

--- guia-ejercicios-php/JSON/aplicacion-19/administrarRemeras.php
+++ guia-ejercicios-php/JSON/aplicacion-19/administrarRemeras.php
@@ -211,11 +211,28 @@
 			
 		case 'modificarRemeras':
 			
-			/*$objetoModificado = isset($_POST['objetoModificar']) ? $_POST['objetoModificar']:NULL;
+			$objetoModificado = isset($_POST['objetoModificar']) ? $_POST['objetoModificar']:NULL;
 			$arrayAux = array();
 			$archivoLectura = fopen("remeras.json", "r");
+			$textJson = "";
 
-			fclose($archivoLectura)	*/
+			//tomo los datos del archivo y los decodeo
+			while (!feof($archivoLectura)) {
+				$textJson .= fgetc($archivoLectura);
+			}
+
+			$miJsonArray = json_decode($textJson);
+
+			//recorremos el array de json para remplazar el objeto por el nuevo
+			foreach ($arrayAux as $objJson) {
+				if ($objJSON->id == $objetoModificar->id) {
+					$objJson = $objetoModificar;
+					echo "Ingreso al foreach";
+					break;
+				}
+			}
+
+			fclose($archivoLectura)
 
 			break;
 	}
